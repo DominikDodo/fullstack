ef3c0f150de0bd9de18f82db67c091bf812628bf
Dominik <spoke111klone@gmail.com>
2023-12-17 20:45:27 +0100

    HUGE UPDATE

---
+++ part1.14/src/App.jsx
@@ -0,0 +1,163 @@
+import { useState } from 'react'
+
+const Button = ({text, onClickHandler}) => (  
+  <button onClick={onClickHandler}>
+    {text}
+  </button>
+)
+
+const ScoreDisplay = ({text, number}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{number}</td>
+  </tr>
+)
+
+const Feedback = ({goodHandler, neutralHandler, badHandler}) => {
+    return (
+      <div>
+        <h1>Give Feedback</h1>
+        <Button text="Good" onClickHandler={goodHandler}/>
+        <Button text="Neutral" onClickHandler={neutralHandler}/>
+        <Button text="Bad" onClickHandler={badHandler}/>
+      </div> 
+    )
+}
+
+const Statistics = ({good, neutral, bad, total, in_order}) => {
+  let positive_percent = (good/(good+bad+neutral))*100 + "%"
+  let average = ((good*1 + bad*-1)/(good+bad))
+  if (good+bad == 0) { // to prevent NaN
+    average = 0
+  }
+  
+  return (
+    <table>
+      <thead><tr><th>Category</th><th>Score</th></tr></thead>
+      <tbody>
+        <ScoreDisplay text="Good" number={good}/>
+        <ScoreDisplay text="Neutral" number={neutral}/>
+        <ScoreDisplay text="Bad" number={bad}/>
+        <ScoreDisplay text="Total rating" number={total}/>
+        <ScoreDisplay text="Average" number={average}/>
+        <ScoreDisplay text="Positive" number={positive_percent}/>
+        <ScoreDisplay text="Order" number={in_order}/>
+      </tbody>
+    </table>
+  )
+}
+
+const Anecdotes = ({anecdotes=[], currentAnecdote, nextHandler, voteHandler, currentAnecdoteScores=[]}) => {
+  return (
+    <>
+      <div>
+        {anecdotes[currentAnecdote]}
+      </div>
+      <Button text={"Vote"} onClickHandler={voteHandler}/>
+      <Button text={"Next Anecdote"} onClickHandler={nextHandler}/>
+      <p>Has {currentAnecdoteScores[currentAnecdote]} votes</p>
+    </>
+  )
+}
+
+const MostVotes = ({anecdotes=[], currentAnecdoteScores=[]}) => {
+  let most_votes = 0;
+  let most_votes_index = 0;
+  
+  for (let i = 0; i < anecdotes.length; i++) {
+    if (currentAnecdoteScores[i] > most_votes) {
+      most_votes = currentAnecdoteScores[i]
+      most_votes_index = i
+    }
+  }
+
+  return (
+    <>
+      <h1>Most voted anecdote</h1>
+      <div>
+        {anecdotes[most_votes_index]}
+      </div>
+      <p>Has {currentAnecdoteScores[most_votes_index]} votes</p>
+    </>
+  )
+}
+
+function App() {
+  ///*
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]  
+  
+  const [currentAnecdote, setCurrentAnecdote] = useState([Math.floor(Math.random() * anecdotes.length), false]) // [random_number, is_first_render?]
+  const [currentAnecdoteScores, setCurrentAnecdoteScores] = useState([0, 0, 0, 0, 0, 0, 0, 0])
+
+  // good/neutral/bad/all reviews
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
+  const [score, setScore] = useState(0) 
+
+  const goodHandler = () => {
+    console.log("Good:", good+1)
+    setGood(good+1)
+    setAll(all.concat('G'))
+    setScore(score+1)
+  }
+  const neutralHandler = () => {
+    console.log("Neutral", neutral+1)
+    setNeutral(neutral+1)
+    setAll(all.concat('N'))
+  }
+  const badHandler = () => {
+    console.log("Evil", bad+1)
+    setBad(bad+1)
+    setAll(all.concat('B'))
+    setScore(score-1)
+  }
+
+  const voteHandler = (an, current) => {
+    return () => {
+      const copy = [...currentAnecdoteScores]
+      copy[an[0]] += 1
+      setCurrentAnecdoteScores(copy)
+    }
+  }
+
+  const nextAnecdoteHandler = () => {
+    let helper = 0
+    let prev = currentAnecdote[0]
+    helper = Math.floor(Math.random() * anecdotes.length)
+    setCurrentAnecdote([helper, true])
+  }
+
+  if (good + bad + neutral) {
+    return (
+      <>
+        <Anecdotes anecdotes={anecdotes} currentAnecdote={currentAnecdote[0]} nextHandler={nextAnecdoteHandler} voteHandler={voteHandler(anecdotes, currentAnecdote)} currentAnecdoteScores={currentAnecdoteScores}/>
+        <MostVotes anecdotes={anecdotes} currentAnecdoteScores={currentAnecdoteScores}/>
+        <Feedback goodHandler={goodHandler} neutralHandler={neutralHandler} badHandler={badHandler}/>
+        <br/>
+        <br/>
+        <Statistics good={good} neutral={neutral} bad={bad} total={score} in_order={all}/>
+      </>
+    )
+  } else {
+    return (
+        <>
+          <Anecdotes anecdotes={anecdotes} currentAnecdote={currentAnecdote[0]} nextHandler={nextAnecdoteHandler} voteHandler={voteHandler((anecdotes, currentAnecdote))} currentAnecdoteScores={currentAnecdoteScores}/>
+          <MostVotes anecdotes={anecdotes} currentAnecdoteScores={currentAnecdoteScores}/>
+          <Feedback goodHandler={goodHandler} neutralHandler={neutralHandler} badHandler={badHandler}/>
+        </>
+    )
+  }
+}
+
+export default App
---
+++ part2/src/App.jsx
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react'
+import Person from './components/Person'
+import Notification from './components/Notification'
+import personService from './services/persons'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+  const [ siteMessage, setSiteMessage] = useState(null)
+  const [ siteMessageType, setSiteMessageType ] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(allPersons => {
+        setPersons(allPersons)
+      })
+  }, [])
+
+  // function to create new name
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      // receives content from the components newName state
+      name: newName,
+      number: newNumber,
+    }
+
+    const existing_names = persons.map(person => person.name)
+
+    if (existing_names.includes(newName)) {
+      const msg = `${newName} is already added to the phonebook. Replace the old number with the new one?`
+      const confirm = window.confirm(msg)
+      if (confirm) {
+        updateName(nameObject)
+      }
+    } else {
+      personService
+        .create(nameObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          resetNewState()
+          setSiteMessage(`Added ${returnedPerson.name}.`)
+          setSiteMessageType('info')
+          setTimeout(() => {
+            setSiteMessage(null)
+          }, 5000)
+        })
+    }
+  }
+
+  const updateName = (nameObject) => {
+    const update_person = persons.find(p => p.name === nameObject.name)
+    const update_id = update_person.id
+    personService
+    .update(update_id, nameObject)
+    .then(returnedPerson =>
+      setPersons(persons.map(person => person.id !== update_id ? person : returnedPerson))
+    )
+    .catch(error => {
+      setSiteMessage(`Note ${nameObject.name} not found. ${error}`)
+      setSiteMessageType('error')
+      setTimeout(() => {
+        setSiteMessage(null)
+      }, 5000)
+      setPersons(persons.filter(p => p.id !== update_id))
+    })
+  }
+
+  const deleteName = (person) => {
+    const msg = `Delete ${person.name}?`
+    const confirm = window.confirm(msg)
+    if (confirm) {
+      personService
+        .deletePerson(person.id)
+        .then(persons =>
+          setPersons(persons)
+    )}
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const resetNewState = () => {
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = newFilter ?
+    persons.filter(person => person.name.toLowerCase().includes(newFilter)) :
+    persons
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification msg={siteMessage} type={siteMessageType} />
+      filter shown with <input type="text" value={newFilter} onChange={handleFilterChange} />
+      <h2>Add new contact</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input  type='text' value={newName} onChange={handleNameChange} /><br />
+          number: <input  type='text' value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {personsToShow.map(person =>
+          <Person
+            key={person.name}
+            person={person}
+            deleteEntry={() => deleteName(person)} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
